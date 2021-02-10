9815c18243cb5606a6ce4381e77d811fc82648fc
Sarah Barton <sarahhtbarton@gmail.com>
2021-02-10 15:54:06 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,12 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    "ssssssssss" + string
   else
     string
   end
 end
+
+puts snake_it_up("surprise!")
