4f4cb7d9744adc7b85cb1da7061e18ce685b3274
Sarah Barton <sarahhtbarton@gmail.com>
2021-02-10 15:47:21 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -4,7 +4,7 @@ require 'pry'
 def start_game(player1, player2)
   "Hello #{player1} & #{player2}"
 end
-start_game("Rocky", "Bullwinkle")
+puts start_game("Rocky", "Bullwinkle")
 
 def play_game(player2)
   "#{player1} is better than #{player2}"
